0906d4f35895026d0b462f0a30eb3614a843abd2
Christina <kiso13@yandex.ru>
2025-06-24 20:13:58 +0300

    мелкие улучшения логики модальных окон

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,24 +2,28 @@ function openPopup(popupElement) {
   popupElement.classList.add("popup_is-opened");
   document.addEventListener("keydown", handleEscape);
 }
+
 function closeModal(modal) {
   modal.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", handleEscape);
 }
+
 function handleEscape(evt) {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector(".popup_is-opened");
-    if (openedPopup) {
+    if (openedPopup !== null) {
       closeModal(openedPopup);
     }
   }
 }
+
 function handleOverlayClick(event) {
-  if (
-    event.target.classList.contains("popup__close") ||
-    event.target === event.currentTarget
-  ) {
+  const isCloseButton = event.target.classList.contains("popup__close");
+  const isOverlayClick = event.target === event.currentTarget;
+
+  if (isCloseButton || isOverlayClick) {
     closeModal(event.currentTarget);
   }
 }
+
 export { openPopup, closeModal, handleEscape, handleOverlayClick };
